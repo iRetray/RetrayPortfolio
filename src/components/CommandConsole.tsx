5c9f9c589fac8da293cdb65433f229e40984cde9
Julian Camilo <julian20141016@gmail.com>
2022-07-19 18:42:10 -0500

    Added console basic interface

--- src/components/CommandConsole.tsx
+++ src/components/CommandConsole.tsx
@@ -1,9 +1,23 @@
 type CommandConsoleProps = {
+  title: string;
   command: string;
+  responseList: string[] | JSX.Element[];
 };
 
-const CommandConsole = ({ command }: CommandConsoleProps): JSX.Element => (
-  <div>Write some commands here</div>
+const CommandConsole = ({
+  title,
+  command,
+  responseList,
+}: CommandConsoleProps): JSX.Element => (
+  <div className="CommandConsoleContainer">
+    <div className="header">
+      <p>{title}</p>
+    </div>
+    <p>{command}</p>
+    {responseList.map((response) => (
+      <p>{response}</p>
+    ))}
+  </div>
 );
 
 export default CommandConsole;
--- src/pages/Presentation.tsx
+++ src/pages/Presentation.tsx
@@ -1,5 +1,7 @@
 import CommandConsole from "../components/CommandConsole";
 
+const responseList: string[] = ["Hola", "Que", "Se", "dice"];
+
 type PresentationProps = {
   name: string;
   role: string;
@@ -9,7 +11,11 @@ const Presentation = ({ name, role }: PresentationProps) => (
   <div className="PresentationContainer centerContainer">
     <p className="titleGradient">{name}</p>
     <p className="description">{role}</p>
-    <CommandConsole command="sudo get-profile --developer=='Julian Camilo'" />
+    <CommandConsole
+      title="home/retray"
+      command="sudo get-profile --developer=='Julian Camilo'"
+      responseList={responseList}
+    />
   </div>
 );
 
